55a687993d859fd933f74e0176445ab21bacf87b
Dhrumil Patel <dhrumilp15@gmail.com>
2019-11-22 17:27:54 -0500

    Updates

---
+++ precache-manifest.112b53edfb02bb929228b3cb029dac42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0a47582f89b83850e491e740a6b383",
+    "url": "/picross/index.html"
+  },
+  {
+    "revision": "7f9dc92cc3127c99626e",
+    "url": "/picross/static/css/main.d3139d39.chunk.css"
+  },
+  {
+    "revision": "42fb681613127af66adc",
+    "url": "/picross/static/js/2.745685cc.chunk.js"
+  },
+  {
+    "revision": "7f9dc92cc3127c99626e",
+    "url": "/picross/static/js/main.cd8abc42.chunk.js"
+  },
+  {
+    "revision": "a5fd2e8b9e3eeeaa68f2",
+    "url": "/picross/static/js/runtime-main.94d080f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/picross/static/media/logo.25bf045c.svg"
+  }
+]);
